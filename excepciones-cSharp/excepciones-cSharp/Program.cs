e874f4cbb2d8b78073d4540e8d2b280c81da03a6
BilliMorales <adn.atf.11@gmail.com>
2022-05-20 11:24:51 -0700

    Exepcciones

---
+++ excepciones-cSharp/excepciones-cSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones_cSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                int num1;
+                int num2;
+                Console.Write("Enter a number: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.Write("enter another number: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+                
+            }
+            catch(FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+        }
+        
+
+    }
+}
